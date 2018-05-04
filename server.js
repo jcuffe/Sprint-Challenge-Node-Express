7df85de0449ae41f3bcf32c124b41f459d93d069
Jared Cuffe <jcuffe@gmail.com>
2018-05-04 09:03:07 -0700

    Bizarre and poorly documented changes to how babel is packaged

--- server.js
+++ server.js
@@ -1,5 +1,34 @@
 import express from 'express'
+import actionModel from './data/helpers/actionModel'
+import projectModel from './data/helpers/projectModel'
 
 const app = express()
+
+const readAll = database => {
+  return (req, res) => {
+    try {
+      const document = database.get()
+      if (document) {
+        res.status(200).send(document)
+      } else {
+        res.status(404).send()
+      }
+    } catch (err) {
+      next(err)
+    }
+  }  
+}
+
+app.get('/', )
+
+// Catch-all error handler
+app.use((err, req, res, next) => {
+  // Defer to default handler if response is partially built
+  if (res.headersSent) {
+    return next(err)
+  }
+  res.status(500)
+  res.render('error', { error: err })
+})
 
 app.listen(5000, console.log('Listening on port 5000'))
